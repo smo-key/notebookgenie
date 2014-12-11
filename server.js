c0336ff29e432d39e7b75d993f7527e1d12f7af6
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-12-11 16:09:39 -0600

    Start server.js, add buttons

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+//instantiate variables
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    app = require("express"),
+    mu = require("mu2"),
+    yaml = require("js-yaml"),
+    trello = require("node-trello")
 
+/* GET PROCESS INFORMATION */
+port = process.argv[2] || 8888; //server port
+configname = process.argv[3] || "_server.yml";
+
+/* READ SERVER CONFIG */
+configdata = fs.readFileSync(configname);
+config = yaml.safeLoad(configdata);
+
+/* SERVER */
+
+
+
+//serve HTTP
