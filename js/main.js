db1aef27baddb6fafbb4b1cfa0153139df2eb2d0
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-12-13 17:07:36 -0600

    Just Left: Fix the modal

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
 $(document).ready(function () {
   firstLoad();
-  //updateContainer();
-  /*$(window).resize(function() {
+  updateContainer();
+  $(window).resize(function() {
     updateContainer();
-  });*/
+  });
 });
 
 function firstLoad()
 {
   //activate tooltips
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
-  //$("#copyright").text('Copyright © ' + new Date().getFullYear().toString() + ' Arthur Pachachura');
+  $('[data-toggle="tooltip"]').tooltip();
+  //make copyright text
+  $("#copyright").html("Trello2LaTeX Copyright © " + new Date().getFullYear().toString() + " Arthur Pachachura<br><b>Licensed under MIT</b>");
 }
 
-/*function updateContainer() {
+function updateContainer() {
   var $cW = $(window).width();
   var $cH = $(window).height();
-  if ($cH < 400) { $cH = 400; }
-  var $stH = $cH;
-  if (($cW < 700) && ($cH > 400)) { $stH = 400; }
-  $(".slidetext-center").css('height',$stH);
-  $(".slidetext-center").css('width',$cW);
-  $(".slidetext-static").css('width',$cW);
-  $(".slidetext-top").css('height',$stH);
-  $(".slidetext-top").css('width',$cW);
-  var $s4H = $cW / 4 - 20;
-  $(".slide4-item").css('height',$s4H);
-  $(".slide4-item").css('padding-top',"0");
-}*/
+
+  //Make window smaller when it needs to be
+  if ($("#loading").height() > $cH)
+  {
+    $("#loading").addClass("centered-top");
+  }
+  else
+  {
+    $("#loading").removeClass("centered-top");
+  }
+
+//  if ($cH < 400) { $cH = 400; }
+//  var $stH = $cH;
+//  if (($cW < 700) && ($cH > 400)) { $stH = 400; }
+//  $(".slidetext-center").css('height',$stH);
+//  $(".slidetext-center").css('width',$cW);
+//  $(".slidetext-static").css('width',$cW);
+//  $(".slidetext-top").css('height',$stH);
+//  $(".slidetext-top").css('width',$cW);
+//  var $s4H = $cW / 4 - 20;
+//  $(".slide4-item").css('height',$s4H);
+//  $(".slide4-item").css('padding-top',"0");
+}
 
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
