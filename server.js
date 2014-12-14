1ba4a5fbf55c2e77c265c21f490f135637bc23c2
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-12-14 12:44:56 -0600

    Get Mustache Working

--- server.js
+++ server.js
@@ -3,21 +3,60 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    app = require("express"),
-    mu = require("mu2"),
+    express = require("express"),
     yaml = require("js-yaml"),
-    trello = require("node-trello")
+    cons = require('consolidate'),
+    logger = require('morgan'),
+    walk = require("walk"),
+    trello = require("node-trello");
+
+//initialize renderer
+var app = express();
+var router = express.Router();
+app.engine('html', cons.mustache);
+
+// set .html as the default extension
+app.set('view engine', 'html');
+app.set('views', __dirname + '/templates');
 
 /* GET PROCESS INFORMATION */
 port = process.argv[2] || 8888; //server port
-configname = process.argv[3] || "_server.yml";
+configname = process.argv[3] || "_private.yml";
 
 /* READ SERVER CONFIG */
 configdata = fs.readFileSync(configname);
 config = yaml.safeLoad(configdata);
 
+/* CREATE MUSTACHE STACHE */
+var stache = { };
+var walker  = walk.walk('./templates', { followLinks: false });
+walker.on('file', function(root, stat, next) {
+  var key   = path.basename(stat.name, path.extname(stat.name));
+  var value = fs.readFileSync(root + '/' + stat.name).toString();
+  stache[key] = value;
+  next();
+});
+walker.on('end', function() {
+//  console.log(stache);
+});
+
 /* SERVER */
+router.use(logger());
 
+app.get('/', function (req, res) {
+  res.render('main', {
+    main: stache.loading
+  });
+});
 
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('404.html', function (req, res) {
+  res.send("404!!!!");
+});
 
 //serve HTTP
+app.listen(port);
