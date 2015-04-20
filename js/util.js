6ddb0a4ec4848ff14c2bb5fab3ee0a562516d58e
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-04-19 22:46:50 -0500

    Remove infinite loops from URL scanning

--- js/util.js
+++ js/util.js
@@ -346,11 +346,11 @@ exports.converttime = function converttime(time) {
 
 exports.mark = function mark(str) {
   var parsemarkdown = true;  //FIXME for now forced to true!
-  if (parsemarkdown || !isnull(str)) {
+  if (parsemarkdown && !isnull(str)) {
     // place the @ character in front of a literal char
     return str.replace(/\*\*(.*)\*\*/igm, '@{@\\bf $1@}') //bold face
     .replace(/\*(.*)\*/igm, '@{@\\emph $1@}') //italics
-    .replace(/^(http(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;=%\$#_]*))?$/igm, '@\\url@{$1@}'); //urls
+    .replace(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/igm, '@\\url@{$1@}'); //urls
   }
   else { return str; }
 }
