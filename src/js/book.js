fa51f1b734f5b8daa97f2f87c370408c364362ec
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-02-26 15:19:34 -0600

    Make basic table of contents

--- src/js/book.js
+++ src/js/book.js
@@ -1,24 +1,34 @@
+/** Table of Contents **/
+
 var ToC =
   "<nav role='navigation' class='toc-inner'>" +
     "<ul>";
 
 var newLine, el, title, link;
 
-$(".title.l1 h1").each(function() {
+$("page .title h1").each(function() {
 
   el = $(this);
   title = el.text();
   link = "#" + el.attr("id");
+  parent = el.parent();
+  classString = "";
+
+  if (parent.hasClass("ignore"))
+    return;
+  else if (parent.hasClass("l1"))
+    classString = "l1";
+  else if (parent.hasClass("l2"))
+    classString = "l2";
 
   newLine =
-    "<li>" +
+    "<li class='" + classString + "'>" +
       "<a href='" + link + "'>" +
         title +
       "</a>" +
     "</li>";
 
   ToC += newLine;
-
 });
 
 ToC +=
