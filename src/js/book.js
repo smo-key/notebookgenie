639cc50b71ef4f83b756e7ce80c6988179df5153
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-02-26 21:56:02 -0600

    Add nearly automatic list content generation

--- src/js/book.js
+++ src/js/book.js
@@ -6,7 +6,7 @@ function populateToC()
     "<nav role='navigation' class='toc-inner'>" +
       "<ul>";
 
-  var newLine, el, title, link;
+  var newLine, el, title, link, classString;
 
   $("page .title h1").each(function() {
 
@@ -40,4 +40,36 @@ function populateToC()
   $(".toc").append(ToC);
 }
 
+function populateList(name)
+{
+  var ToC =
+    "<nav role='navigation' class='list-cards-inner' id='list-" + name + "-cards-inner'>" +
+      "<ul>";
+
+  var newLine, el, title, link;
+
+  $("page.card.list-" + name + " .title h1").each(function() {
+
+    el = $(this);
+    title = el.text();
+    link = "#" + el.attr("id");
+
+    newLine =
+      "<li>" +
+        "<a href='" + link + "'>" +
+          title +
+        "</a>" +
+      "</li>";
+
+    ToC += newLine;
+  });
+
+  ToC +=
+     "</ul>" +
+    "</nav>";
+
+  $("page.list#list-" + name + "-page .list-cards").append(ToC);
+}
+
 populateToC();
+populateList("software");
